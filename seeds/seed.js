0422ea794115afcded75c3a02f21347d9d4f17cf
uyo <jaypushcode@gmail.com>
2025-06-11 00:34:18 -0400

    Adding subtle db test and fixes seed after manual db creation

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const sequelize = require('../config/connection');
 const { User, JobPost, Events } = require('../models');
 
@@ -6,24 +8,27 @@ const jobData = require('./jobData.json');
 const eventsData = require('./eventsData.json');
 
 const seedDatabase = async () => {
+  console.log('Connecting to DB...');
+  await sequelize.authenticate();
+
+  console.log('Connected successfully.');
+
+  console.log('Syncing models...');
   await sequelize.sync({ force: true });
+  console.log('Models synced.');
 
   const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+      individualHooks: true,
+      returning: true,
+    });
 
   for (const job of jobData) {
-    await JobPost.create({
-      ...job,
-    });
-  }
+      await JobPost.create({ ...job });
+    }
 
   for (const event of eventsData) {
-    await Events.create({
-      ...event,
-    });
-  }
+      await Events.create({ ...event });
+    }
 
   process.exit(0);
 };
